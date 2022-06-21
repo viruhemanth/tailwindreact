5a3dc949a0dde89af17b942f3d03ccc48b4627a7
Hemanth Kumar S <viruhemanth@gmail.com>
2022-06-21 09:51:00 +0530

    tailwindreact template for testing

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# CHANGELOG.md
+
+## [1.1.0] - 2021-10-20
+Update dependencies and remove some
+
+## [1.0.2] - 2021-08-19
+
+Fix mobile menu issue
+
+## [1.0.1] - 2021-05-10
+
+Update dependencies
+
+## [1.0.0] - 2020-12-18
+
+First release
---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.scss';
+
+import AOS from 'aos';
+import { focusHandling } from 'cruip-js-toolkit';
+
+import Home from './pages/Home';
+import Testimonials from './pages/Testimonials';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Help from './pages/Help';
+import PageNotFound from './pages/PageNotFound';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 750,
+      easing: 'ease-out-quart',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+    focusHandling('outline');
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/testimonials">
+          <Testimonials />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/blog-post">
+          <BlogPost />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/help">
+          <Help />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
